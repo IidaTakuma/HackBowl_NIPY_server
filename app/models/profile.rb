04aa764811d56e0ec546e45109def92c222a5904
User Name <ta9maxxx@gmail.com>
2019-10-12 14:38:15 +0900

    setting validate Profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,7 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :nickname, presence: true, length: { maximum: 50 }
+  validates :icon_path, presence: true
+  validates :skills, presence: true, length: { maximum: 255 }
 end
