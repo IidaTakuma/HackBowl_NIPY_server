3ac55047c1cbeae0698fb53ab66a4edc6ac23070
User Name <ta9maxxx@gmail.com>
2019-10-12 13:46:35 +0900

    create User Profile model/setting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#index'
-  get 'homes/index'
+
+  resources :users, only: %i[index show edit]
+  resources :profile, only: %i[index show edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_10_12_043102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "profiles", force: :cascade do |t|
+    t.string "name"
+    t.string "nickname"
+    t.string "icon_path"
+    t.string "skills"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "profiles", "users"
 end
