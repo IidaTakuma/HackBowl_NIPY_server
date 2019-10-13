6e1c9ff2825c2ea0336d8222bb2aa750b926f0df
User Name <ta9maxxx@gmail.com>
2019-10-13 13:09:52 +0900

    set basic function signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'homes#index'
-
-  resources :users, only: %i[index show edit]
+  resources :users, only: %i[new create index show edit]
   resources :profile, only: %i[index show edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
