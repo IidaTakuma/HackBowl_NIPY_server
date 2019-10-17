f17ff36d375acda8cf613d6c4069960c03bc50de
User Name <ta9maxxx@gmail.com>
2019-10-17 11:43:10 +0900

    メールアドレスとパスワード投げたらトークンが帰ってくるようになった

--- app/controllers/api_sessions_controller.rb
+++ app/controllers/api_sessions_controller.rb
@@ -1,8 +1,10 @@
 class ApiSessionsController < ApplicationController
+  skip_before_action :require_login, only: %i[login]
+  skip_before_action :require_profile, only: %i[login]
   def login
-    @user = User.find_by(email: params[:email].downcase)
+    user = User.find_by(email: params[:email].downcase)
     if user&.authenticate(params[:password])
-      render json: @user.token
+      render json: user.token
     else
       render json: 'no auth'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'api_sessions/login'
   root 'homes#index'
   resources :users, only: %i[new create index show edit update]
   resources :profiles, only: %i[new create show edit update]
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Use for API
+  get 'api_sessions/login'
 end
