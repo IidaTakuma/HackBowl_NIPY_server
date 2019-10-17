3a6e9472e581db5d7248b0c36f0e2fa986033216
User Name <ta9maxxx@gmail.com>
2019-10-17 10:29:51 +0900

    add token column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_token
+
   has_one :profile
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
