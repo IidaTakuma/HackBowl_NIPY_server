57a9a2c113539ecb518f4bfa39c69a57cd594269
User Name <ta9maxxx@gmail.com>
2019-10-18 13:02:36 +0900

    settings routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'homes#index'
   resources :users, only: %i[new create index show edit update]
   resources :profiles, only: %i[new create show edit update]
+  resources :follows, only: %i[new create]
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
