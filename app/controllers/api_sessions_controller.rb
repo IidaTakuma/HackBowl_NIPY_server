bdbfbced2ade02b82df740b9a1a9527f5df2b867
User Name <ta9maxxx@gmail.com>
2019-10-19 23:55:53 +0900

    Webの名刺一覧リストを作成した

--- app/controllers/api_sessions_controller.rb
+++ app/controllers/api_sessions_controller.rb
@@ -4,20 +4,25 @@ class ApiSessionsController < ApplicationApiController
   def login
     user = User.find_by(email: params[:email].downcase)
     if user&.authenticate(params[:password])
-      # プロフィールがない場合のバリデーションを実装
-      profile = Profile.find_by(user: user)
-      render json: { 'token': user.token,
-                     'user_id': user.id,
-                     'name': profile.name,
-                     'nickname': profile.nickname,
-                     'skills': profile.skills,
-                     'affiliation1': profile.affiliation1,
-                     'affiliation2': profile.affiliation2,
-                     'Github': profile.githubID,
-                     'Qiita': profile.QiitaID,
-                     'otherURL': profile.otherURL }
+      if !user.profile.nil?
+        profile = Profile.find_by(user: user)
+        render json: { 'token': user.token,
+                       'user_id': user.id,
+                       'name': profile.name,
+                       'nickname': profile.nickname,
+                       'skills': profile.skills,
+                       'affiliation1': profile.affiliation1,
+                       'affiliation2': profile.affiliation2,
+                       'Github': profile.githubID,
+                       'Qiita': profile.QiitaID,
+                       'otherURL': profile.otherURL }
+      else
+        render json: { 'status': 'ERROR',
+                       'message': 'profile not found' }
+      end
     else
-      render json: { 'error': 'no auth' }
+      render json: { 'status': 'ERROR',
+                     'message': 'no auth' }
     end
   end
 end
